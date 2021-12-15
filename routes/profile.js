d74910df9fc94aed50af81a1bf7e33db8beb1daa
Derek Dhammaloka <41344434+derektypist@users.noreply.github.com>
2021-12-15 16:48:27 +0000

    Added route to profile at profile.js

--- routes/profile.js
+++ routes/profile.js
@@ -5,6 +5,22 @@ const Poll = require('../models').Poll;
 const User = require('../models').User;
 
 // Set Up Routes
+router.get('/', isLoggedIn, function(req, res) {
+  console.log(req.user);
+  Poll.find({"createdBy": req.user._id}, function(err, polls) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+
+    res.render('profile', {
+      title: "profile",
+      polls: polls
+    });
+  });
+});
+
+
 
 // Login Function
 function isLoggedIn(req, res, next) {
